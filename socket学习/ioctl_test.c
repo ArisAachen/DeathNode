c7d5335993e5857b109c9ebe218db4c844fe72fd
Aris AAchen <chenyunxiong@uniontech.com>
2021-09-26 17:59:49 +0800

    add af_route

--- socket学习/ioctl_test.c
+++ socket学习/ioctl_test.c
@@ -16,10 +16,13 @@
 
 
 #include <math.h>
-
+#include <arpa/inet.h>
 #include "genernal_define.h"
 #include <net/if.h>
+#include <net/if_arp.h>
 #include <sys/ioctl.h>
+
+
 
 // ifconfig
 void get_ifconfig_test() {
@@ -52,8 +55,71 @@ void get_ifconfig_test() {
 
 }
 
+// ifreq
+void get_ifreq_test() {
+
+    char if_name[IFNAMSIZ] = "eno1";
+    struct ifreq req;
+
+    memset(&req, 0, sizeof(struct ifreq));
+    req.ifr_addr.sa_family = AF_INET;
+    strncpy(req.ifr_name, if_name, IFNAMSIZ);
+
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1)
+        bad_exit("socket");
+
+    if (ioctl(fd, SIOCGIFADDR, &req) == -1)
+        bad_exit("ifaddr");
+
+    if (req.ifr_addr.sa_family == AF_INET) {
+        char buf[INET_ADDRSTRLEN];
+        struct sockaddr_in *ipv4_addr = (struct sockaddr_in *) &req.ifr_addr;
+        const char *addr = inet_ntop(AF_INET, &ipv4_addr->sin_addr, buf, INET_ADDRSTRLEN);
+        if (addr == NULL)
+            bad_exit("inet");
+        printf("addr is %s \n", addr);
+    } else {
+        bad_exit("unknown protocol");
+    }
+
+    if (ioctl(fd, SIOCGIFFLAGS, &req) == -1)
+        bad_exit("flags");
+
+    if (req.ifr_flags & IFF_BROADCAST) {
+        printf("BROADCAST ");
+    }
+
+    if(req.ifr_flags & IFF_MULTICAST) {
+        printf("MULTICAST \n");
+    }
+
+}
+
+void arp_test() {
+    struct arpreq req;
+    struct sockaddr_in* addr;
+
+    strncpy(req.arp_dev,"eno1",IFNAMSIZ);
+    addr = (struct sockaddr_in*)&req.arp_pa;
+
+    addr->sin_family = AF_INET;
+    if(inet_pton(AF_INET,"10.20.43.117",&addr->sin_addr) == -1)
+        bad_exit("inet");
+
+    int fd = socket(AF_INET,SOCK_DGRAM,0);
+    if(fd == -1)
+        bad_exit("socket");
+    if(ioctl(fd,SIOCGARP,&req) == -1)
+        bad_exit("ioctl");
+
+    printf("family is %d \n",req.arp_ha.sa_family);
+    char *ptr = &req.arp_ha.sa_data[0];
+    printf("hwadr is %x:%x:%x:%x:%x:%x \n",*ptr,*(ptr+1),*(ptr+2),*(ptr+3),*(ptr+4),*(ptr+5));
+}
+
 int main() {
-    get_ifconfig_test();
+    arp_test();
 
     return 1;
 }
